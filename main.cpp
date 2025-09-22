2d6115b76f656555a851560deb4c0782fbabcc87
Edwin Piedrahita <edwin.piedrahita1@udea.edu.co>
2025-09-22 22:27:51 +0200

    Se implementaron las funciones de rotar bits a la derecha y desencriptar el mensaje

--- main.cpp
+++ main.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!" << endl;
+unsigned char rotarDerecha(unsigned char byte, int posiciones);
+
+int main() {
+
+
     return 0;
+}
+
+//Función para rotar los bits a la derecha
+unsigned char rotarDerecha(unsigned char byte, int posiciones) {
+    return (byte >> posiciones) | (byte << (8 - posiciones));
+}
+
+// Función para desencriptar el mensaje
+char* desencriptar(char* mensajeEncriptado, int longitud, int rotacion, unsigned char claveXor) {
+    char* desencriptado = new char[longitud + 1];
+
+    for (int i = 0; i < longitud; i++) {
+        unsigned char temp = (unsigned char)mensajeEncriptado[i] ^ claveXor;
+        desencriptado[i] = (char)rotarDerecha(temp, rotacion);
+    }
+
+    desencriptado[longitud] = '\0';
+    return desencriptado;
 }
