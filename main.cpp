351cf2844f11961b5d0928f29f6cb037229910bc
Edwin Piedrahita <edwin.piedrahita1@udea.edu.co>
2025-09-24 23:53:49 +0200

    Se implementaron las funciones para descomprimir por LZ78 y para verificar si el texto que se recupero contiene esa pista

--- main.cpp
+++ main.cpp
@@ -6,9 +6,24 @@ using namespace std;
 unsigned char rotarDerecha(unsigned char byte, int posiciones);
 char* desencriptar(char* mensajeEncriptado, int longitud, int rotacion, unsigned char claveXor);
 char* descomprimirRLE(const char* mensajeComprimido, int longitud, int& longitudSalida);
+char* descomprimirLZ78(const unsigned char* mensajeComprimido, int longitud, int& longitudSalida);
+bool contieneFragmento(const char* texto, int longitudTexto, const char* fragmento, int longitudFragmento);
 
 int main() {
 
+    const char* texto = "ABAABABA";
+    int longitudTexto = 8;
+
+    const char* fragmento = "AABA";
+    int longitudFragmento = 4;
+
+    if (contieneFragmento(texto, longitudTexto, fragmento, longitudFragmento)) {
+        cout << "El fragmento SI se encuentra en el texto." << endl;
+    } else {
+        cout << "El fragmento NO se encuentra en el texto." << endl;
+    }
+
+    cin.get();
 
     return 0;
 }
@@ -72,8 +87,79 @@ char* descomprimirRLE(const char* mensajeComprimido, int longitud, int& longitud
     return descomprimido;
 }
 
+//Función para descomprimir por LZ78
+char* descomprimirLZ78(const unsigned char* mensajeComprimido, int longitud, int& longitudSalida) {
+    int capacidadDiccionario = 1000;
+    char** diccionario = new char*[capacidadDiccionario];
+    int* longitudes = new int[capacidadDiccionario];
+    int entradas = 0;
 
+    char* salida = new char[longitud * 10];
+    int posSalida = 0;
 
+    for (int i = 0; i < longitud; i += 3) {
 
+        int indice = (mensajeComprimido[i] << 8) | mensajeComprimido[i + 1];
 
+        unsigned char c = mensajeComprimido[i + 2];
 
+        int lenPrefijo = 0;
+        if (indice > 0 && indice <= entradas) {
+            lenPrefijo = longitudes[indice - 1];
+        }
+
+        char* nuevo = new char[lenPrefijo + 1 + 1];
+
+        if (indice > 0 && indice <= entradas) {
+            for (int j = 0; j < lenPrefijo; j++) {
+                nuevo[j] = diccionario[indice - 1][j];
+            }
+        }
+
+        nuevo[lenPrefijo] = c;
+        nuevo[lenPrefijo + 1] = '\0';
+
+        diccionario[entradas] = nuevo;
+        longitudes[entradas] = lenPrefijo + 1;
+        entradas++;
+
+        for (int j = 0; j < lenPrefijo + 1; j++) {
+            salida[posSalida++] = nuevo[j];
+        }
+    }
+
+    salida[posSalida] = '\0';
+    longitudSalida = posSalida;
+
+    for (int i = 0; i < entradas; i++) {
+        delete[] diccionario[i];
+    }
+    delete[] diccionario;
+    delete[] longitudes;
+
+    return salida;
+}
+
+//Función para verificar si el texto contiene ese fragmento
+bool contieneFragmento(const char* texto, int longitudTexto, const char* fragmento, int longitudFragmento) {
+    if (longitudFragmento > longitudTexto) {
+        return false;
+    }
+
+    for (int i = 0; i <= longitudTexto - longitudFragmento; i++) {
+        bool coincide = true;
+
+        for (int j = 0; j < longitudFragmento; j++) {
+            if (texto[i + j] != fragmento[j]) {
+                coincide = false;
+                break;
+            }
+        }
+
+        if (coincide) {
+            return true;
+        }
+    }
+
+    return false;
+}
